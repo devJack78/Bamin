de91e12698444858d5a13ac0cd4934b84eada649
devJack78 <43338011+devJack78@users.noreply.github.com>
2022-07-08 10:22:07 +0900

    bbbbbb

---
+++ Bamin/File2.swift
@@ -0,0 +1,8 @@
+//
+//  File2.swift
+//  Bamin
+//
+//  Created by jack on 2022/07/08.
+//
+
+import Foundation
---
+++ Bamin/File3.swift
@@ -0,0 +1,8 @@
+//
+//  File3.swift
+//  Bamin
+//
+//  Created by jack on 2022/07/08.
+//
+
+import Foundation
